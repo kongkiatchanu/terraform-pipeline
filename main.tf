40e373d71d3ebe34e1ffb357da4d8720894c509d
kongkiatchanu <kongkiatchanu@gmail.com>
2021-03-19 17:45:31 +0700

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+	region = "ap-southeast-1"
+}
+
+variable "ami" {
+	default = "ami-0619d309766d26749"
+}
+
+
+resource "aws_instance" "my-instance" {
+        ami = "ami-0619d309766d26749" 
+        instance_type = "t3a.micro"
+        key_name = "thaidevelopers-aws"
+	tags = {
+		Name = "TEST-Terraform"	
+	}
+}
