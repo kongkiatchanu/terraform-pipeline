bd7ca98768747571a9a63120328a3a214d7dbe25
kongkiatchanu <54658160+kongkiatchanu@users.noreply.github.com>
2021-03-19 17:58:29 +0700

    update

--- main.tf
+++ main.tf
@@ -1,17 +1,18 @@
 provider "aws" {
-	region = "ap-southeast-1"
+        region = "ap-southeast-1"
 }
 
 variable "ami" {
-	default = "ami-0619d309766d26749"
+        default = "ami-0619d309766d26749"
 }
 
 
 resource "aws_instance" "my-instance" {
-        ami = "ami-0619d309766d26749" 
+        ami = "ami-0619d309766d26749"
         instance_type = "t3a.micro"
         key_name = "thaidevelopers-aws"
-	tags = {
-		Name = "TEST-Terraform"	
-	}
+        tags = {
+                Name = "TEST-Terraform"
+        }
 }
+
